b81c48abba80af45dafe738a9fc66ef2fcd8635f
LIMA MOHANTY <chandraodgroup>
2025-08-09 13:33:13 +0530

    bus seatlayout check for bus running

--- src/app/constants/global-constants.ts
+++ src/app/constants/global-constants.ts
@@ -4,14 +4,17 @@ import { Injectable } from "@angular/core";
 export class GlobalConstants {  
     public static ismobile: boolean= true;
    
-   public static BASE_URL:any ='https://consumer.odbus.co.in/api';
-   public static URL:any ='https://www.odbus.in/';
-   public static PAYMENT_MODE:any ='production';
+   // public static BASE_URL:any ='https://consumer.odbus.co.in/api';
+   // public static URL:any ='https://www.odbus.in/';
+   // public static PAYMENT_MODE:any ='production';
    
 //    public static BASE_URL:any ='https://testing.odbus.co.in/api';
 //    public static URL:any ='https://odtesting.odbus.co.in/';
-//    public static PAYMENT_MODE:any ='sandbox';
+   public static PAYMENT_MODE:any ='sandbox';
    //public static URL:any ='http://localhost:4200/';
+
+   public static BASE_URL:any ='https://localhost/api';
+    public static URL:any ='http://localhost:4200/';
 
    public static USER_ID:any = "";
    public static MASTER_SETTING_USER_ID:any = "1";
