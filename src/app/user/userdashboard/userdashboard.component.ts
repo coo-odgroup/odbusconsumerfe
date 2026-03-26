03c0b5bb0df71b2ed3b99295615df71b95707e07
siraz <siraz.odbus@gmail.com>
2026-03-26 17:45:49 +0530

    Progress Bar Added

--- src/app/user/userdashboard/userdashboard.component.ts
+++ src/app/user/userdashboard/userdashboard.component.ts
@@ -1,8 +1,8 @@
-import { Component,Input,OnInit } from '@angular/core';
-import { UserdataService  } from '../../services/userdata.service';
+import { Component, Input, OnInit } from '@angular/core';
+import { UserdataService } from '../../services/userdata.service';
 import { NgxSpinnerService } from "ngx-spinner";
 import { NotificationService } from '../../services/notification.service';
-import { NgbDatepickerConfig,NgbModal,NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
+import { NgbDatepickerConfig, NgbModal, NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { LocationdataService } from '../../services/locationdata.service';
 import { Router } from '@angular/router';
 import { LoginChecker } from '../../helpers/loginChecker';
@@ -16,22 +16,28 @@ import { DatePipe } from '@angular/common';
   selector: 'app-userdashboard',
   templateUrl: './userdashboard.component.html',
   styleUrls: ['./userdashboard.component.css'],
-  providers: [NgbActiveModal,DatePipe]
+  providers: [NgbActiveModal, DatePipe]
 })
 export class UserdashboardComponent implements OnInit {
 
-  list:any=[];
-  per_page=5;
+  activeRefund: string | null = null;
 
-  
-  @Input() session: LoginChecker; 
+  toggleRefund(pnr: string) {
+    this.activeRefund = this.activeRefund === pnr ? null : pnr;
+  }
 
-  statusLabel:any='';
-  profile:any;
-  cancelInfo:any;
+  list: any = [];
+  per_page = 5;
 
-  pnr:any;
-  bus_id:any;
+
+  @Input() session: LoginChecker;
+
+  statusLabel: any = '';
+  profile: any;
+  cancelInfo: any;
+
+  pnr: any;
+  bus_id: any;
 
   OverallRate = 0;
   ComfortRate = 0;
@@ -39,19 +45,19 @@ export class UserdashboardComponent implements OnInit {
   BehaviourRate = 0;
   TimingRate = 0;
 
-  
+
   reviewForm: FormGroup;
   submitted = false;
-  user:any;
+  user: any;
 
   isMobile: boolean;
-  MenuActive:boolean = false;
-  currentDate:any;
+  MenuActive: boolean = false;
+  currentDate: any;
   activeMenu: string;
 
   constructor(
     private userdataservice: UserdataService,
-    private spinner: NgxSpinnerService,    
+    private spinner: NgxSpinnerService,
     private notify: NotificationService,
     private dtconfig: NgbDatepickerConfig,
     private router: Router,
@@ -62,241 +68,234 @@ export class UserdashboardComponent implements OnInit {
     public fb: FormBuilder,
     private deviceService: DeviceDetectorService,
     private datePipe: DatePipe
-    ) 
-       { 
-          this.currentDate =this.datePipe.transform((new Date), 'yyyy-MM-dd'); 
-          //console.log(this.currentDate);
-          this.isMobile = this.deviceService.isMobile();
-          this.session = new LoginChecker();
-          this.user=JSON.parse(localStorage.getItem('user'));
-          
-          this.getList();
-          this.profileData();
+  ) {
+    this.currentDate = this.datePipe.transform((new Date), 'yyyy-MM-dd');
+    //console.log(this.currentDate);
+    this.isMobile = this.deviceService.isMobile();
+    this.session = new LoginChecker();
+    this.user = JSON.parse(localStorage.getItem('user'));
 
-          const current = new Date();
-            this.dtconfig.minDate = { year: current.getFullYear(), month: 
-            current.getMonth() + 1, day: current.getDate() };
+    this.getList();
+    this.profileData();
 
-            this.reviewForm = this.fb.group({
-              comments: ['', Validators.required],
-              title: ['', Validators.required]
-            })        
-       }
-      
+    const current = new Date();
+    this.dtconfig.minDate = {
+      year: current.getFullYear(), month:
+        current.getMonth() + 1, day: current.getDate()
+    };
 
-     menu(){
-      this.MenuActive = (this.MenuActive==false) ? true : false;  
-        this.activeMenu='';       
-     }
+    this.reviewForm = this.fb.group({
+      comments: ['', Validators.required],
+      title: ['', Validators.required]
+    })
+  }
 
-     signOut(){
-      this.session.logout();
-      this.router.navigate(['login']);   
-    }
-       
-   viewTicket(pnr:any){
 
-    const request= {
-      "pnr":pnr,
-      "mobile":this.profile.phone
-     };   
-     
+  menu() {
+    this.MenuActive = (this.MenuActive == false) ? true : false;
+    this.activeMenu = '';
+  }
 
-     this.managebookingService.getbookingdetails(request).subscribe(
-      res=>{ 
-        if(res.status==1){          
-          localStorage.setItem('bookingDetails',JSON.stringify(res.data[0]));
-          this.router.navigate(['manage-booking-detail']);            
-        } 
-        if(res.status==0){
-          this.notify.notify(res.message,"Error");
-        } 
+  signOut() {
+    this.session.logout();
+    this.router.navigate(['login']);
+  }
+
+  viewTicket(pnr: any) {
+
+    const request = {
+      "pnr": pnr,
+      "mobile": this.profile.phone
+    };
+
+
+    this.managebookingService.getbookingdetails(request).subscribe(
+      res => {
+        if (res.status == 1) {
+          localStorage.setItem('bookingDetails', JSON.stringify(res.data[0]));
+          this.router.navigate(['manage-booking-detail']);
+        }
+        if (res.status == 0) {
+          this.notify.notify(res.message, "Error");
+        }
 
         this.spinner.hide();
       },
-    error => {
-      this.spinner.hide();
-      this.notify.notify(error.error.message,"Error");
-    });
+      error => {
+        this.spinner.hide();
+        this.notify.notify(error.error.message, "Error");
+      });
 
-   }
+  }
 
-   profileData(){
-    
+  profileData() {
+
     this.spinner.show();
-    this.profile=JSON.parse(localStorage.getItem('user'));
+    this.profile = JSON.parse(localStorage.getItem('user'));
 
     this.session = new LoginChecker();
-    this.userdataservice.getProfile(this.profile.id,this.profile.token).subscribe(
-      res=>{
-        if(res.status==0)
-        { 
+    this.userdataservice.getProfile(this.profile.id, this.profile.token).subscribe(
+      res => {
+        if (res.status == 0) {
           this.session.logout();
-          this.router.navigate(['login']); 
+          this.router.navigate(['login']);
         }
-      }); 
+      });
 
-   }
+  }
 
-   getList(url:any='',status:any=''){
+  getList(url: any = '', status: any = '') {
 
     this.spinner.show();
 
-    this.statusLabel=status;
-    const param= {
-      "status":status,
-      "paginate":this.per_page,
-      "userId":this.user.id,
-      "token":this.user.token
-     };
+    this.statusLabel = status;
+    const param = {
+      "status": status,
+      "paginate": this.per_page,
+      "userId": this.user.id,
+      "token": this.user.token
+    };
 
-    
-     this.userdataservice.BookingHistroy(url,param).subscribe(
-      res=>{ 
-        if(res.status==1)
-        {             
-            this.list = res.data.data;
-            //console.log(this.list);
+
+    this.userdataservice.BookingHistroy(url, param).subscribe(
+      res => {
+        if (res.status == 1) {
+          this.list = res.data.data;
+          //console.log(this.list);
         }
         this.spinner.hide();
 
       },
-    error => {
-      this.spinner.hide();
-      //this.notify.notify("Login is expired","Error");
-    }
+      error => {
+        this.spinner.hide();
+        //this.notify.notify("Login is expired","Error");
+      }
     );
-   }
+  }
 
 
-   page(label:any){
+  page(label: any) {
     return label;
-   }
+  }
 
-   
-  cancel_ticket(){
+
+  cancel_ticket() {
     this.spinner.show();
-    const request= {
-      "pnr":this.pnr,
-      "phone":this.profile.phone
-     };
+    const request = {
+      "pnr": this.pnr,
+      "phone": this.profile.phone
+    };
 
-     
+
     this.managebookingService.cancelTicket(request).subscribe(
-      res=>{ 
-        
-        if(res.status==1){          
-          this.notify.notify(res.message,"Success");   
+      res => {
+
+        if (res.status == 1) {
+          this.notify.notify(res.message, "Success");
           this.getList();
           this.modalService.dismissAll();
-        } 
-
-        if(res.status==0){
-          this.notify.notify(res.message,"Error");
         }
-        
+
+        if (res.status == 0) {
+          this.notify.notify(res.message, "Error");
+        }
+
         this.spinner.hide();
 
       },
-    error => {
-      this.spinner.hide();
-      this.notify.notify(error.error.message,"Error");
-    }
+      error => {
+        this.spinner.hide();
+        this.notify.notify(error.error.message, "Error");
+      }
     );
 
 
   }
 
-   cancelTicketTab(pnr:any,content:any) {   
+  cancelTicketTab(pnr: any, content: any) {
 
-    this.pnr=pnr;
+    this.pnr = pnr;
     this.spinner.show();
 
-    const request= {
-      "pnr":this.pnr,
-      "mobile":this.profile.phone
-     };
+    const request = {
+      "pnr": this.pnr,
+      "mobile": this.profile.phone
+    };
 
     this.managebookingService.getcancelTicketInfo(request).subscribe(
-      res=>{
+      res => {
 
-        if(res.status==1)
-        {
-            if(typeof res.data ==='string')
-            {
-                this.notify.notify(res.data,"Error");
-            }
+        if (res.status == 1) {
+          if (typeof res.data === 'string') {
+            this.notify.notify(res.data, "Error");
+          }
 
-            if(typeof res.data ==='object')
-            {
-                this.open(content);
-                this.cancelInfo=res.data; 
-            }              
+          if (typeof res.data === 'object') {
+            this.open(content);
+            this.cancelInfo = res.data;
+          }
         }
 
-        if(res.status==0){
-          this.notify.notify(res.message,"Error");
+        if (res.status == 0) {
+          this.notify.notify(res.message, "Error");
         }
 
-        this.spinner.hide();         
+        this.spinner.hide();
 
       },
-    error => {
-      this.spinner.hide();
-      this.notify.notify(error.error.message,"Error");
-    }
+      error => {
+        this.spinner.hide();
+        this.notify.notify(error.error.message, "Error");
+      }
     );
   }
 
-  open(content:any) {
+  open(content: any) {
     this.modalService.open(content);
   }
 
-  
-  sourceData:any;
-  destinationData:any;
-  bookAgain(sr:any,ds:any){
-  this.spinner.show();    
+
+  sourceData: any;
+  destinationData: any;
+  bookAgain(sr: any, ds: any) {
+    this.spinner.show();
 
     this.locationService.all().subscribe(
-      res=>{
-        if(res.status==1)
-        { 
-          res.data.filter((itm) =>{          
+      res => {
+        if (res.status == 1) {
+          res.data.filter((itm) => {
 
-            if(sr===itm.name){
-             this.sourceData=itm;
+            if (sr === itm.name) {
+              this.sourceData = itm;
             }
 
-            if(ds===itm.name){
-              this.destinationData=itm;
+            if (ds === itm.name) {
+              this.destinationData = itm;
             }
 
           });
 
-          let dt=(<HTMLInputElement>document.getElementById("todayDate")).value;
-          this.listing(this.sourceData,this.destinationData,dt);
+          let dt = (<HTMLInputElement>document.getElementById("todayDate")).value;
+          this.listing(this.sourceData, this.destinationData, dt);
 
         }
-      });  
+      });
 
-      this.spinner.hide();     
+    this.spinner.hide();
 
   }
 
-  listing(s:any,d:any,dt: any)
-  {  
-      this.locationService.setSource(s);
-      this.locationService.setDestination(d);
-      this.locationService.setDate(dt);    
-      this.router.navigate(['/listing']);
+  listing(s: any, d: any, dt: any) {
+    this.locationService.setSource(s);
+    this.locationService.setDestination(d);
+    this.locationService.setDate(dt);
+    this.router.navigate(['/listing']);
   }
 
-  addReview(pnr:any,bus_id:any,review:any)
-  {
-      this.pnr=pnr;
-      this.bus_id=bus_id;
-      this.modalService.open(review, { size: 'lg' });
+  addReview(pnr: any, bus_id: any, review: any) {
+    this.pnr = pnr;
+    this.bus_id = bus_id;
+    this.modalService.open(review, { size: 'lg' });
 
   }
 
@@ -306,18 +305,18 @@ export class UserdashboardComponent implements OnInit {
 
     this.submitted = true;
 
-     // stop here if form is invalid
-     if (this.reviewForm.invalid) {
+    // stop here if form is invalid
+    if (this.reviewForm.invalid) {
       return;
-     }else{ 
+    } else {
 
       this.spinner.show();
-       
-       const reviewData= {
+
+      const reviewData = {
         "pnr": this.pnr,
-        "bus_id":this.bus_id,
+        "bus_id": this.bus_id,
         "users_id": this.profile.id,
-        "reference_key": (this.profile.email==null) ? this.profile.phone: this.profile.email ,
+        "reference_key": (this.profile.email == null) ? this.profile.phone : this.profile.email,
         "rating_overall": this.OverallRate,
         "rating_comfort": this.ComfortRate,
         "rating_clean": this.CleanRate,
@@ -325,33 +324,32 @@ export class UserdashboardComponent implements OnInit {
         "rating_timing": this.TimingRate,
         "comments": this.reviewForm.value.comments,
         "title": this.reviewForm.value.title,
-        "user_id":GlobalConstants.MASTER_SETTING_USER_ID,
+        "user_id": GlobalConstants.MASTER_SETTING_USER_ID,
         "created_by": this.profile.name
-       };
+      };
 
       // console.log(reviewData);
-     
-       this.userdataservice.addreview(reviewData).subscribe(
-        res=>{ 
-      // console.log(res);
-         if(res.status==1)
-         {
-              this.modalService.dismissAll();
-              this.notify.notify(res.message,"Success");
-              this.router.navigate(['/my-reviews']);
-         }
 
-         this.spinner.hide();         
+      this.userdataservice.addreview(reviewData).subscribe(
+        res => {
+          // console.log(res);
+          if (res.status == 1) {
+            this.modalService.dismissAll();
+            this.notify.notify(res.message, "Success");
+            this.router.navigate(['/my-reviews']);
+          }
+
+          this.spinner.hide();
         },
-      error => {
-        this.spinner.hide();
-        this.notify.notify(error.error,"Error");
-      }
-      );        
-     }   
+        error => {
+          this.spinner.hide();
+          this.notify.notify(error.error, "Error");
+        }
+      );
+    }
   }
 
   ngOnInit(): void {
-    
+
   }
 }
