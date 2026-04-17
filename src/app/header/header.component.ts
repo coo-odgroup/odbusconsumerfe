b8c113a4a842734527559f840de898906b1c4bce
siraz <siraz.odbus@gmail.com>
2026-04-17 18:25:30 +0530

    add top app download bar

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -4,102 +4,93 @@ import { LoginChecker } from '../helpers/loginChecker';
 import { Title, Meta } from '@angular/platform-browser';
 import { SeoService } from '../services/seo.service';
 import { CommonService } from '../services/common.service';
-import{ GlobalConstants } from '../constants/global-constants';
+import { GlobalConstants } from '../constants/global-constants';
 import { Location } from '@angular/common';
 import { DeviceDetectorService } from 'ngx-device-detector';
 import { isPlatformBrowser } from '@angular/common';
 import { Subscription } from 'rxjs';
 
-
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  //templateUrl:GlobalConstants.ismobile? './header.component.mobile.html':'./header.component.html',
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit, OnDestroy {
+
   @Input() masterSettingRecord;
-   @Input() session: LoginChecker; 
+  @Input() session: LoginChecker;
 
-    collapsed = true;    
-    user:any={};
-    url_path = '';
-    logo_image='';
+  collapsed = true;
+  user: any = {};
+  logo: any = '';
+  isMobile: boolean = false;
 
-    meta_title = '';
-    meta_keyword = '';
-    meta_description = '';
+  // ✅ TOP BAR CONTROL
+  showTopBar: boolean = true;
 
-    seolist:any;
-    currentUrl:any;
-    logo:any='';
-    isMobile:boolean = false; // Default to false for SSR
-    private commonDataSubscription: Subscription;
+  private commonDataSubscription: Subscription;
 
-    constructor( 
-      private router: Router,
-      private titleService: Title, 
-      private commonService: CommonService,
-      private metaService: Meta,
-      private seo:SeoService,
-      location: Location,
-      private deviceService: DeviceDetectorService,
-      @Inject(PLATFORM_ID) private platformId: Object
-    ) { 
-      // Only detect device in browser, default to false (desktop) for SSR
-      this.isMobile = isPlatformBrowser(this.platformId) ? this.deviceService.isMobile() : false;
-        
-      
-      this.session = new LoginChecker();  
-      this.currentUrl = location.path().replace('/','');
-      
-      // Initialize logo from current commonData if available
-      this.updateLogoFromCommonData();
+  constructor(
+    private router: Router,
+    private titleService: Title,
+    private commonService: CommonService,
+    private metaService: Meta,
+    private seo: SeoService,
+    location: Location,
+    private deviceService: DeviceDetectorService,
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) {
+
+    this.isMobile = isPlatformBrowser(this.platformId)
+      ? this.deviceService.isMobile()
+      : false;
+
+    this.session = new LoginChecker();
+    this.updateLogoFromCommonData();
+  }
+
+  updateLogoFromCommonData() {
+    this.masterSettingRecord = this.commonService.commonData;
+
+    if (this.masterSettingRecord && this.masterSettingRecord.common) {
+      this.logo = this.masterSettingRecord.common.logo_image || '';
+    } else {
+      this.logo = '';
     }
+  }
 
-    updateLogoFromCommonData() {
-      this.masterSettingRecord = this.commonService.commonData;
-      // Safely access logo with null check for SSR
-      if (this.masterSettingRecord && this.masterSettingRecord.common) {
-        this.logo = this.masterSettingRecord.common.logo_image || '';
-      } else {
-        this.logo = '';
+  ngAfterContentChecked() {
+    this.updateLogoFromCommonData();
+  }
+
+  ngOnInit(): void {
+    this.user = this.session.getUser();
+
+    this.commonDataSubscription = this.commonService.commonData$.subscribe(data => {
+      if (data) {
+        this.masterSettingRecord = data;
+        this.updateLogoFromCommonData();
       }
+    });
+
+    this.updateLogoFromCommonData();
+  }
+
+  ngOnDestroy(): void {
+    if (this.commonDataSubscription) {
+      this.commonDataSubscription.unsubscribe();
     }
+  }
 
-    ngAfterContentChecked(){
-      this.updateLogoFromCommonData();
+  signOut() {
+    if (this.session.isLoggedIn()) {
+      this.session.logout();
+      this.router.navigate(['login']);
     }
+  }
 
-    ngOnInit(): void {
-      this.user = this.session.getUser();
-      
-      // Subscribe to commonData changes
-      this.commonDataSubscription = this.commonService.commonData$.subscribe(data => {
-        if (data) {
-          this.masterSettingRecord = data;
-          this.updateLogoFromCommonData();
-        }
-      });
-      
-      // Also check current value immediately
-      this.updateLogoFromCommonData();
-    }
-
-    ngOnDestroy(): void {
-      if (this.commonDataSubscription) {
-        this.commonDataSubscription.unsubscribe();
-      }
-    }
-
-    signOut(){
-       if(this.session.isLoggedIn()){
-        this.session.logout();
-        this.router.navigate(['login']);  
-      }
-       
-    }
-
-
-
+  // ✅ CLOSE BAR (only hides until refresh)
+  closeTopBar() {
+    this.showTopBar = false;
+  }
 }
