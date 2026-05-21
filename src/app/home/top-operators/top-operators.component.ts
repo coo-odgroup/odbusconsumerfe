e9f761206ebdd71f902346d0c6e0cee98a89f492
Jagannath Das <jagandas.odbus@gmail.com>
2026-05-21 14:43:24 +0530

    operator details , menu, home page

--- src/app/home/top-operators/top-operators.component.ts
+++ src/app/home/top-operators/top-operators.component.ts
@@ -11,8 +11,13 @@ export class TopOperatorsComponent implements OnInit {
   constructor(private router: Router) { }
 
   topOperators: any = [];
+  displayedOperators: any = [];
+  showAllOperators = false;
+  isMobile = false;
 
   ngOnInit(): void {
+    this.isMobile = window.innerWidth <= 768;
+
     try {
       const data = localStorage.getItem('PopularInfo');
 
@@ -21,11 +26,20 @@ export class TopOperatorsComponent implements OnInit {
 
         if (parsedData && parsedData.topOperators) {
           this.topOperators = parsedData.topOperators;
+
+          this.displayedOperators = this.isMobile
+            ? this.topOperators.slice(0, 5)
+            : this.topOperators;
         }
       }
     } catch (error) {
       console.error('Error parsing localStorage data', error);
     }
+  }
+
+  viewAllOperators() {
+    this.showAllOperators = true;
+    this.displayedOperators = this.topOperators;
   }
 
   operator_detail(url: any) {
