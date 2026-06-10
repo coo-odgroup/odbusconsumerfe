216077ae907f230d74f781e03d13d157d1982e82
sahilprogrammer <sahil.odbus@gmail.com>
2026-06-10 17:49:10 +0530

    dashboard update

--- src/app/contact-us/contact-us.component.ts
+++ src/app/contact-us/contact-us.component.ts
@@ -27,84 +27,88 @@ export class ContactUsComponent implements OnInit {
   session: LoginChecker;
   activeMenu?: string;
   @Input() masterSettingRecord;
-  constructor( public router: Router,
+
+  captchaText: string = '';
+  userCaptcha: string = '';
+
+  constructor(public router: Router,
     public fb: FormBuilder,
-    private contactService: ContactService,    
-    private notify: NotificationService, 
+    private contactService: ContactService,
+    private notify: NotificationService,
     private spinner: NgxSpinnerService,
-    private seo:SeoService,
+    private seo: SeoService,
     private deviceService: DeviceDetectorService,
     private location: Location,
     private commonService: CommonService,
 
-    ) { 
+  ) {
 
-      this.isMobile = this.deviceService.isMobile();
-      this.session = new LoginChecker();
-      this.currentUrl = location.path().replace('/','');
-      this.seo.seolist(this.currentUrl);
-      
+    this.isMobile = this.deviceService.isMobile();
+    this.session = new LoginChecker();
+    this.currentUrl = location.path().replace('/', '');
+    this.seo.seolist(this.currentUrl);
 
-      this.contactForm = this.fb.group({
-        name: ['', Validators.required],
-        email: ['', Validators.required],
-        phone: ['', [Validators.required,Validators.pattern("^[0-9]{10}$")]],
-        service: ['', Validators.required],
-        message: ['', Validators.required],
-        user_id:[GlobalConstants.MASTER_SETTING_USER_ID]
-      })
-    }
 
-    menu() {
-      this.MenuActive = (this.MenuActive==false) ? true : false;  
-      this.activeMenu='';    
-    }
-  
-    signOut() {
-        this.session.logout();
-        this.router.navigate(['login']);
-    }
+    this.contactForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', Validators.required],
+      phone: ['', [Validators.required, Validators.pattern("^[0-9]{10}$")]],
+      service: ['', Validators.required],
+      message: ['', Validators.required],
+      user_id: [GlobalConstants.MASTER_SETTING_USER_ID]
+    })
+  }
 
-    ngAfterContentChecked(){
-      
-      //console.log(this.masterSettingRecord.common.office_address_map);
-    }
-    onlyNumbers(event:any) {
-      var e = event ;
-      var charCode = e.which || e.keyCode;
-     
-        if ((charCode >= 48 && charCode <= 57) || (charCode >= 96 && charCode <= 105) || charCode ==8 || charCode==9)
-          return true;
-          return false;        
+  menu() {
+    this.MenuActive = (this.MenuActive == false) ? true : false;
+    this.activeMenu = '';
+  }
+
+  signOut() {
+    this.session.logout();
+    this.router.navigate(['login']);
+  }
+
+  ngAfterContentChecked() {
+
+    //console.log(this.masterSettingRecord.common.office_address_map);
+  }
+  onlyNumbers(event: any) {
+    var e = event;
+    var charCode = e.which || e.keyCode;
+
+    if ((charCode >= 48 && charCode <= 57) || (charCode >= 96 && charCode <= 105) || charCode == 8 || charCode == 9)
+      return true;
+    return false;
   }
 
   onSubmit() {
 
     this.submitted = true;
 
-     // stop here if form is invalid
-     if (this.contactForm.invalid) {
+    // stop here if form is invalid
+    if (this.contactForm.invalid) {
       return;
-     }else{ 
+    } else {
       this.spinner.show();
       this.contactService.save(this.contactForm.value).subscribe(
-        res=>{ 
-          this.spinner.hide();         
-          if(res.status==1){  
+        res => {
+          this.spinner.hide();
+          if (res.status == 1) {
             this.router.navigate(['thankyou']);
           }
-          else if(res.status==0){
-            this.notify.notify(res.message,"Error");
+          else if (res.status == 0) {
+            this.notify.notify(res.message, "Error");
           }
           this.spinner.hide();
         },
         error => {
           this.spinner.hide();
-          this.notify.notify(error.error.message,"Error");
+          this.notify.notify(error.error.message, "Error");
         }
-      );        
-     }
-   
+      );
+    }
+
   }
 
 
@@ -112,8 +116,30 @@ export class ContactUsComponent implements OnInit {
 
   ngOnInit(): void {
 
+    this.generateCaptcha();
+
     this.masterSettingRecord = this.commonService.commonData;
     console.log(this.masterSettingRecord.common.office_address_map);
   }
 
+  generateCaptcha() {
+    const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    let result = '';
+
+    for (let i = 0; i < 6; i++) {
+      result += chars.charAt(Math.floor(Math.random() * chars.length));
+    }
+
+    this.captchaText = result;
+  }
+
+  validateCaptcha() {
+    if (this.userCaptcha === this.captchaText) {
+      alert('Captcha matched');
+    } else {
+      alert('Invalid captcha');
+      this.generateCaptcha();
+    }
+
+  }
 }
