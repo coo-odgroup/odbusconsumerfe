553ac312faefd2c0c4dbb30488b5a2c41d8b12a8
Jagannath Das <jagan.odbus@gmail.com>
2026-06-23 18:22:21 +0530

    offer section slider change

--- src/app/home/odbus-offers/odbus-offers.component.ts
+++ src/app/home/odbus-offers/odbus-offers.component.ts
@@ -94,10 +94,10 @@ export class OdbusOffersComponent implements OnInit {
           this.Offers = [];
         }
 
-        this.offerList = this.Offers.map((item) => ({
-          path: item.slider_photo,
-          alt: item.alt_tag
-        }));
+        // this.offerList = this.Offers.map((item) => ({
+        //   path: item.slider_photo,
+        //   alt: item.alt_tag
+        // }));
 
         // console.log(this.Offers);
       },
@@ -124,5 +124,43 @@ export class OdbusOffersComponent implements OnInit {
     this.router.navigate(['/offers'], {
       fragment: couponCode,
     });
+  }
+
+  moveSlider(direction: number) {
+    const slider: any = document.getElementById('cardsScroll');
+    if (!slider) return;
+
+    const scrollAmount = 420; // card move distance
+
+    slider.scrollTo({
+      left: slider.scrollLeft + direction * scrollAmount,
+      behavior: 'smooth',
+    });
+
+    setTimeout(() => {
+      this.updateSliderButtons();
+    }, 500);
+  }
+
+  updateSliderButtons() {
+    const slider: any = document.getElementById('cardsScroll');
+    const prevBtn: any = document.querySelector('.prev-btn');
+    const nextBtn: any = document.querySelector('.next-btn');
+
+    if (!slider || !prevBtn || !nextBtn) return;
+
+    /* LEFT BUTTON */
+    if (slider.scrollLeft <= 5) {
+      prevBtn.style.display = 'none';
+    } else {
+      prevBtn.style.display = 'flex';
+    }
+
+    /* RIGHT BUTTON */
+    if (slider.scrollLeft + slider.clientWidth >= slider.scrollWidth - 5) {
+      nextBtn.style.display = 'none';
+    } else {
+      nextBtn.style.display = 'flex';
+    }
   }
 }
