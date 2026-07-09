d9555c6177529753d4f72161c1c0a9bbbfa33c31
Chakradhar <chakradhar@odbus.in>
2026-07-09 15:02:10 +0530

    updated

--- src/app/constants/global-constants.ts
+++ src/app/constants/global-constants.ts
@@ -17,12 +17,21 @@ export class GlobalConstants {
   // public static PATHURL: any = 'https://testingadminapi.odbus.co.in/public/uploads/';
 
   // Testing
-  public static BASE_URL: any = 'https://testing.odbus.co.in/api';
+  // public static BASE_URL: any = 'https://testing.odbus.co.in/api';
   // public static URL: any = 'https://odtesting.odbus.co.in/';
-  public static URL: any = 'http://localhost:4200/';
+  // public static URL: any = 'http://192.168.29.151:4200/';
   // public static URL: any = 'http://192.168.29.85:4200/';
+  // public static PAYMENT_MODE: any = 'sandbox';
+  // public static PATHURL: any = 'https://testingadminapi.odbus.co.in/public/uploads/';
+
+ // Testing ADGLOB
+  public static BASE_URL: any = 'https://odclient.adglob.in/api';
+  public static URL: any = 'https://odbus.adglob.in/';
   public static PAYMENT_MODE: any = 'sandbox';
-  public static PATHURL: any = 'https://testingadminapi.odbus.co.in/public/uploads/';
+  public static PATHURL: any = 'https://odapi.adglob.in/public/uploads/';
+
+
+
 
   // Local
   // public static BASE_URL: any = 'http://localhost:7001/ODBUS/odbusconsumerbe/api';
