37b64a857ea9c2b00e928cd67e00941518eadfb4
siraz <siraz.odbus@gmail.com>
2026-09-21 12:43:43 +0530

    New Payment Comfirmation Added

--- src/app/new-page/new-page.component.ts
+++ src/app/new-page/new-page.component.ts
@@ -7,9 +7,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewPageComponent implements OnInit {
 
+  customerName = 'SK TABRIZ ALLI';
+
+  pnrNumber = 'ODM21036588';
+
+  phone = '9937874390';
+
+  email = 'coo@odgroup.in';
+
+  copied = false;
+
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(): void { }
+
+  viewTicket(): void {
+    navigator.clipboard.writeText(this.pnrNumber).then(() => {
+      this.copied = true;
+
+      setTimeout(() => {
+        this.copied = false;
+      }, 2000);
+    });
   }
 
+  continueToHomepage(): void {
+    window.location.href = '/';
+  }
 }
